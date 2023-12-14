799eb5aa586fb6c97deac6ef7da8474bb146a266
Daniel Shulgin <danielshulgin37@gmail.com>
2023-12-14 12:43:05 +0100

    add test connection to spring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/user/0');
+        const data = await response.json();
+        setApiData(data); // Update the state with API data
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Awesome App</h1>
+      {apiData ? (
+        // Render your data here, adjust based on your DTO structure
+        <div>
+          <p>Data from API:</p>
+          <p>{apiData.name}</p>
+          {/* Add more properties as needed */}
+        </div>
+      ) : (
+        // Show a loading spinner or message while data is being fetched
+        <p>Loading...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
